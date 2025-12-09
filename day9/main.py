126927f559ce6acf3f0d209855433955b55f47ad
willzhao <gnomeek@outlook.com>
2025-12-09 17:47:46 +0800

    day9 pt2

--- day9/main.py
+++ day9/main.py
@@ -1,17 +1,18 @@
 import sys
+from collections import deque
 
-class Dot(object):
+
+class Dot:
     def __init__(self, x, y):
         self.x = int(x)
         self.y = int(y)
 
     def diff_x(self, another: 'Dot'):
         return abs(self.x - another.x)
-    
 
     def diff_y(self, another: 'Dot'):
         return abs(self.y - another.y)
-    
+
 
 def read_input(file_path: str) -> list[Dot]:
     pos = []
@@ -32,7 +33,60 @@ def solve_pt1(positions: list[Dot]) -> int:
 
 
 def solve_pt2(positions: list[Dot]) -> int:
-    return 0
+    n = len(positions)
+
+    xs = sorted(set([0, sys.maxsize] + [p.x for p in positions]))
+    ys = sorted(set([0, sys.maxsize] + [p.y for p in positions]))
+
+    shrink_x = {x: i for i, x in enumerate(xs)}
+    shrink_y = {y: i for i, y in enumerate(ys)}
+
+    width, height = len(xs), len(ys)
+    grid = [['X'] * width for _ in range(height)]
+
+    for i in range(n):
+        x1, y1 = positions[i].x, positions[i].y
+        x2, y2 = positions[(i + 1) % n].x, positions[(i + 1) % n].y
+        cx1, cy1 = shrink_x[x1], shrink_y[y1]
+        cx2, cy2 = shrink_x[x2], shrink_y[y2]
+        for cx in range(min(cx1, cx2), max(cx1, cx2) + 1):
+            for cy in range(min(cy1, cy2), max(cy1, cy2) + 1):
+                grid[cy][cx] = '#'
+    queue = deque([(0, 0)])
+    grid[0][0] = '.'
+
+    while queue:
+        cy, cx = queue.popleft()
+        for dy, dx in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            ny, nx = cy + dy, cx + dx
+
+            if 0 <= ny < height and 0 <= nx < width and grid[ny][nx] == 'X':
+                grid[ny][nx] = '.'
+                queue.append((ny, nx))
+    res = 0
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            x1, y1 = positions[i].x, positions[i].y
+            x2, y2 = positions[j].x, positions[j].y
+
+            cx1, cy1 = shrink_x[x1], shrink_y[y1]
+            cx2, cy2 = shrink_x[x2], shrink_y[y2]
+
+            valid = True
+            for cx in range(min(cx1, cx2), max(cx1, cx2) + 1):
+                for cy in range(min(cy1, cy2), max(cy1, cy2) + 1):
+                    if grid[cy][cx] == '.':
+                        valid = False
+                        break
+                if not valid:
+                    break
+
+            if valid:
+                area = (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
+                res = max(res, area)
+
+    return res
 
 
 if __name__ == "__main__":
