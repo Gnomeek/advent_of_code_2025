0d29bc756b5fec350ae885f37fb6cf683d5b104e
willzhao <gnomeek@outlook.com>
2025-12-09 15:46:04 +0800

    day9 pt1

--- day9/main.py
+++ day9/main.py
@@ -1,25 +1,46 @@
 import sys
 
-def read_input(file_path: str):
+class Dot(object):
+    def __init__(self, x, y):
+        self.x = int(x)
+        self.y = int(y)
+
+    def diff_x(self, another: 'Dot'):
+        return abs(self.x - another.x)
+    
+
+    def diff_y(self, another: 'Dot'):
+        return abs(self.y - another.y)
+    
+
+def read_input(file_path: str) -> list[Dot]:
+    pos = []
     with open(file_path, "r") as file:
-        pass
+        for line in file.readlines():
+            vals = line.strip().split(",")
+            pos.append(Dot(vals[0], vals[1]))
+    return pos
 
-
-def solve_pt1(grid: list[list[str]]) -> int:
-    res = 0
+def solve_pt1(positions: list[Dot]) -> int:
+    res, n = 0, len(positions)
+    for i in range(n):
+        for j in range(i+1, n):
+            width = positions[i].diff_x(positions[j]) + 1
+            height = positions[i].diff_y(positions[j]) + 1
+            res = max(res, width*height)
     return res
 
-def solve_pt2(grid: list[list[str]]) -> int:
-    res = 0
-    return res
+
+def solve_pt2(positions: list[Dot]) -> int:
+    return 0
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "test":
-        grid = read_input("input_test.txt")
+        positions = read_input("input_test.txt")
     else:
-        grid = read_input("input.txt")
-    result_pt1 = solve_pt1(grid)
-    result_pt2 = solve_pt2(grid)
+        positions = read_input("input.txt")
+    result_pt1 = solve_pt1(positions)
+    result_pt2 = solve_pt2(positions)
     print(f"Part 1: {result_pt1}")
     print(f"Part 2: {result_pt2}")
