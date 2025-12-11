642be8da24b360ff929ea276f338f536f171df14
willzhao <gnomeek@outlook.com>
2025-12-11 13:11:09 +0800

    day11 skeleton

--- day11/main.py
+++ day11/main.py
@@ -1,25 +1,29 @@
+from collections import defaultdict
 import sys
 
 
-def read_input(file_path: str):
+def read_input(file_path: str) -> dict[str, list[str]]:
+    graph = defaultdict(list)
     with open(file_path, "r") as file:
-        pass
-    return []
+        for line in file.readlines():
+            src, dsts = line.strip().split(": ")
+            graph[src] = dsts.split(" ")
+    return graph
 
-def solve_pt1(x) -> int:
+def solve_pt1(graph: dict[str, list[str]]) -> int:
     return 0
 
 
-def solve_pt2(x) -> int:
+def solve_pt2(graph: dict[str, list[str]]) -> int:
     return 0
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "test":
-        x = read_input("input_test.txt")
+        graph = read_input("input_test.txt")
     else:
-        x = read_input("input.txt")
-    result_pt1 = solve_pt1(x)
-    result_pt2 = solve_pt2(x)
+        graph = read_input("input.txt")
+    result_pt1 = solve_pt1(graph)
+    result_pt2 = solve_pt2(graph)
     print(f"Part 1: {result_pt1}")
     print(f"Part 2: {result_pt2}")
