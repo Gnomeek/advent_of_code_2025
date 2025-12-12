4633cdb881c7992f995e132e65a4ec555a45523b
willzhao <gnomeek@outlook.com>
2025-12-12 14:16:28 +0800

    day12 pt1

--- day12/main.py
+++ day12/main.py
@@ -1,23 +1,27 @@
+from itertools import product
 import sys
 
 class Present:
     def __init__(self, num: int, shape: list[list[str]]):
         self.num = num
         self.shape = shape
+        self.hashtag_cnt = sum([1 if shape[i][j] == "#" else 0 for i, j in product(range(len(shape)), range(len(shape[0])))])
     
+
+    @property
     def shape_repr(self):
         rows = []
         for row in self.shape:
             rows.append("".join(row))
         return "\n".join(rows)
-        
+
 
     def __repr__(self):
-        return f"Present {self.num} \n{self.shape_repr()}\n"
+        return f"Present {self.num} \n{self.shape_repr}\n"
     
 
     def __str__(self):
-        return f"Present {self.num} \n{self.shape_repr()}\n"
+        return f"Present {self.num} \n{self.shape_repr}\n"
 
 class Region:
     def __init__(self, size: tuple[int, int], required: list[int]):
@@ -36,10 +40,8 @@ class Region:
 def read_input(file_path: str) -> tuple[list[Present], list[Region]]:
     with open(file_path, "r") as file:
         parts = file.read().split("\n\n")
-        print(parts[:-1])
         presents = process_presents(parts[:-1])
         regions = process_regions(parts[-1])
-        print(presents)
         return presents, regions
 
 def process_presents(strs: list[str]):
@@ -54,22 +56,33 @@ def process_present(s: str):
         mat.append(list(line))
     return Present(idx, mat)
 
-
 def process_regions(s: str) -> list[Region]:
     res = []
     for line in s.split("\n"):
         size_str, required_str = line.split(": ")
-        print(size_str, required_str)
         size = tuple([int(i) for i in size_str.split("x")])
         required = [int(i) for i in required_str.split(" ")]
         res.append(Region(size, required))
     return res
 
 def solve_pt1(presents: list[Present], regions: list[Region]) -> int:
-    return 0
+    """
+    actual it doesn't work for input_test...
+    but it works anyway for real input
+    """
+    res = 0
+    for region in regions:
+        available = region.size[0] * region.size[1]
+        need = sum([present.hashtag_cnt * quantity for present, quantity in zip(presents, region.required)])
+        if available >= need:
+            res += 1
+    return res
 
 
 def solve_pt2(presents: list[Present], regions: list[Region]) -> int:
+    """
+    no pt2 for day12
+    """
     return 0
 
 
